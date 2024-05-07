f6d04cfc668d922903450e15aae984216112dfbe
sabarisan-s <iamsabarisan@gmail.com>
2024-05-07 15:09:02 +0530

    updated pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import SlideImg from "../components/Section/SlideImg";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    return (
+        <>
+            <Navbar />
+            <SlideImg />
+        </>
+    );
+};
 
-export default Home
+export default Home;
